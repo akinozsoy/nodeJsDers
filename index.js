bd6e722b0a4c9677a7dc1bba18105a96f67bdc15
Akın Özsoy <akn.ozsy5757@gmail.com>
2022-10-13 12:42:31 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// var http= require("http");
+// var fs= require("fs");
+
+// var server = http.createServer((req,res ) =>{
+//     if(req.url == "/"){
+//         fs.readFile("index.html",(err,html) =>{
+//             res.write(html);
+//             res.end();
+            
+//         });
+
+//     }else if(req.url == "/products"){
+//         fs.readFile("urunler.html",(err,html) =>{
+//             res.write(html);
+//             res.end();
+//         });
+//     }else{
+//         fs.readFile("404.html" ,(err,html) =>{
+//             res.write(html);
+//             res.end();
+//         });
+    
+//     }
+// });
+
+// server.listen(3000, () =>{
+//   https://www.youtube.com/watch?v=0hZNdTogNo0  console.log("node.js server at port 3000");
+// });
+
+
+
+const express = require("express");
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+const data = [
+    {id: 1, name: "iphone 14",price:30000, isActive: true, imageUrl: "1.jpg"},
+    {id: 2, name: "iphone 15",price:40000, isActive: false, imageUrl: "2.jpg"},
+    {id: 3, name: "iphone 16",price:50000, isActive: true, imageUrl: "3.png"},
+    {id: 4, name: "iphone 17 Plus",price:60000, isActive: false}
+];
+
+
+app.use("/products/:id", function(req,res){
+    const urun = data.find(u => u.id == req.params.id);
+    res.render("product-details",urun);
+});
+app.use("/products", function(req,res){
+    res.render("products",{
+        urunler:data
+    });
+});
+app.use("/", function(req,res){
+    res.render("index");
+});
+
+app.listen(3000,() =>{
+    console.log("Listening on 3000 port ")
+});
